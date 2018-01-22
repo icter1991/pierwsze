39d94ee0da28e563abecd76682a9b42448fb419a
int <shifu@costam.edu.pl>
2018-01-22 23:32:26 +0000

    Minor changes
    Editing comments

--- program.py
+++ program.py
@@ -16,13 +16,14 @@ def get_users(host_id):
     r = requests.get('http://{}'.format(host_id), auth=auth)
     string_of_users = r.json()
     for element in string_of_users:
-        return("ID: {} \t NAME: {}".format(element['id'], element['name']))
 
+        return("ID: {} \t NAME: {}".format(element['id'], element['name']))
 
 def post_users(host_id, name, password):
     # Adds new user
     payload = {'name': name, 'password': password}
     r = requests.post('http://{}'.format(host_id), json=payload)
+
     return r.status_code == 200
 
 def get_user_id(host_id, name):
@@ -33,20 +34,18 @@ def get_user_id(host_id, name):
     for element in string_of_users:
         list_of_code.append(element['name'])
         if name in list_of_code:
+
             return element['id']
 
-
 def put_user_id(host_id, user_id, name, password):
-
+    # Modifies users password
     payload = {'name': name, 'password': password}
     r = requests.put('http://{}/{}'.format(host_id, user_id), auth=auth, data=json.dumps(payload))
 
     return r.status_code == 200
 
-
-
 def delete_user(host_id, user_id, name, password):
-
+    # Extinguishes user for ever and ever!!!
     payload = {'name': name, 'password': password}
     r = requests.delete('http://{}/{}'.format(host_id, user_id), auth=auth, data=json.dumps(payload))
 
