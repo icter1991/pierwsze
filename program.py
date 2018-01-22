7c22d5a1e129b23c0133d788412af07696aeda04
int <shifu@costam.edu.pl>
2018-01-22 23:33:54 +0000

    Minor changes
    xxx

--- program.py
+++ program.py
@@ -11,16 +11,17 @@ def authorisation_id():
 
 
 def get_users(host_id):
-    #Gets all users
+    # Gets all users
 
     r = requests.get('http://{}'.format(host_id), auth=auth)
     string_of_users = r.json()
     for element in string_of_users:
 
-        return("ID: {} \t NAME: {}".format(element['id'], element['name']))
+        return "ID: {} \t NAME: {}".format(element['id'], element['name'])
 
 def post_users(host_id, name, password):
     # Adds new user
+
     payload = {'name': name, 'password': password}
     r = requests.post('http://{}'.format(host_id), json=payload)
 
@@ -28,6 +29,7 @@ def post_users(host_id, name, password):
 
 def get_user_id(host_id, name):
     # Gets users ID after typing it's name
+
     list_of_code = []
     r = requests.get('http://{}'.format(host_id), auth=auth)
     string_of_users = r.json()
@@ -39,6 +41,7 @@ def get_user_id(host_id, name):
 
 def put_user_id(host_id, user_id, name, password):
     # Modifies users password
+
     payload = {'name': name, 'password': password}
     r = requests.put('http://{}/{}'.format(host_id, user_id), auth=auth, data=json.dumps(payload))
 
@@ -46,6 +49,7 @@ def put_user_id(host_id, user_id, name, password):
 
 def delete_user(host_id, user_id, name, password):
     # Extinguishes user for ever and ever!!!
+
     payload = {'name': name, 'password': password}
     r = requests.delete('http://{}/{}'.format(host_id, user_id), auth=auth, data=json.dumps(payload))
 
